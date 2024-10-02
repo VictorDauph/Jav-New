dda23c71b3c9e31412183f8101602cb67543df13
VictorDauph <79511345+VictorDauph@users.noreply.github.com>
2024-10-02 19:34:12 +0200

    ex1 init

--- chapitre1-Modules/exercice1/Ex1/data-repository/src/main/java/UserRepository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package UserRepository;
-
-public class UserRepository {
-
-	public UserRepository() {
-		System.out.println("User Repository Built");
-	}
-	
-}
---
+++ chapitre1-Modules/exercice1/graphics/src/graphicWindows/StartWindow.java
@@ -0,0 +1,13 @@
+package graphicWindows;
+
+import dataRepository.userRepository.UserRepository;
+
+public class StartWindow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("StartWindow Launched");
+		UserRepository userRepository = new UserRepository();
+	}
+
+}
