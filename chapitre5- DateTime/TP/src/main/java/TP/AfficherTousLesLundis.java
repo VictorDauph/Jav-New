b4989251b10b54014f0ee8ca8c6119825749b555
VictorDauph <79511345+VictorDauph@users.noreply.github.com>
2024-10-08 22:06:06 +0200

    ajout Tp DateTime

---
+++ chapitre5- DateTime/TP/src/main/java/TP/AfficherTousLesLundis.java
@@ -0,0 +1,24 @@
+package TP;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.YearMonth;
+import java.time.temporal.TemporalAdjusters;
+
+public class AfficherTousLesLundis {
+    public static void main(String[] args) {
+        int annee = 2023; // Année choisie
+        Month mois = Month.MARCH; // Mois choisi
+
+        YearMonth yearMonth = YearMonth.of(annee, mois);
+        LocalDate premierJour = yearMonth.atDay(1);
+        LocalDate premierLundi = premierJour.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
+
+        System.out.println("Tous les lundis de " + mois + " " + annee + " :");
+        while (premierLundi.getMonth() == mois) {
+            System.out.println(premierLundi);
+            premierLundi = premierLundi.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+        }
+    }
+}
