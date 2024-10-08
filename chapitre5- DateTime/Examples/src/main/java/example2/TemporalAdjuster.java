8230bcbd56e444570f603a05021be165ec5ad5ac
VictorDauph <79511345+VictorDauph@users.noreply.github.com>
2024-10-08 21:59:24 +0200

    etat initial DateTime

--- chapitre5- DateTime/Examples/src/main/java/example2/TemporalAdjuster.java
+++ chapitre5- DateTime/Examples/src/main/java/example2/TemporalAdjuster.java
@@ -8,12 +8,10 @@ public class TemporalAdjuster {
 	
 	public static void main(String[] args) {
 		//Trouver le dernier jour d'une année
-		LocalDate lastDayOfYear = LocalDate.of(2000, 12, 1).with(TemporalAdjusters.lastDayOfYear());
-		System.out.println(lastDayOfYear);
+
 		
 		//trouver si l'année est après 2000
-		Boolean isYearAfter2000 = LocalDate.now().query(t-> t.get(ChronoField.YEAR) > 2000);
-		System.out.println(isYearAfter2000);
+
 
 	}
 }
