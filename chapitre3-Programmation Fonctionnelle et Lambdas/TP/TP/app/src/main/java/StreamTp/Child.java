c60e12ddb05fe50403402dba55485cc544149abf
VictorDauph <79511345+VictorDauph@users.noreply.github.com>
2024-10-10 13:13:16 +0200

    correction TP

--- chapitre3-Programmation Fonctionnelle et Lambdas/TP/TP/app/src/main/java/StreamTp/Child.java
+++ chapitre3-Programmation Fonctionnelle et Lambdas/TP/TP/app/src/main/java/StreamTp/Child.java
@@ -1,10 +1,27 @@
 package StreamTp;
 
 public class Child {
+    private String name;
+    private int age;
 
+    public Child(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 
     @Override
     public String toString() {
-
+        return "Child{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
