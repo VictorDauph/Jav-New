9ec5b07c01a1ed3d7c5d865dcee18840ea1fb8ae
VictorDauph <79511345+VictorDauph@users.noreply.github.com>
2024-10-10 13:36:59 +0200

    correction exercice switch

--- chapitre4- Switchs et Patterns/Exercice1/src/main/java/ancien/Ancien.java
+++ chapitre4- Switchs et Patterns/Exercice1/src/main/java/ancien/Ancien.java
@@ -4,6 +4,29 @@ import enums.*;
 public class Ancien {
 	   
 	    public static void main(String[] args) {
-	       
+	        int statut = 2;
+	        boolean bool = true;
+	        StatusEnum statusEnum = StatusEnum.UNDEFINED;  // Valeur par défaut
+
+	        switch (statut) {
+	            case 1:
+	                statusEnum = StatusEnum.OK;
+	                break;
+	            case 0:
+	                statusEnum = StatusEnum.KO;
+	                break;
+	            case 2:
+	            	//on ne peut pas switch sur un bool
+	                if (bool) {
+	                    statusEnum = StatusEnum.OK;
+	                } else {
+	                    statusEnum = StatusEnum.KO;
+	                }
+	                break;
+	            default:
+	                statusEnum = StatusEnum.UNDEFINED;
+	        }
+
+	        System.out.println("Status Enum: " + statusEnum);
 	    }
 }
